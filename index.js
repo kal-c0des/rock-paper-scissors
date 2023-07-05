305c0ca82aa90fb39fc4d29bb1567adfbc24e5ad
Kalsie Holt <kalsie@student.dev-island.org>
2023-07-04 22:34:27 -1000

    Edit styles.css, add resetGame function, add button disable to endGame function

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ let roundCount = 0;
 
 let mainGame = document.querySelector("main");
 
+let messageText = document.querySelector("div.message p");
+let resetText = document.querySelector("div.reset p");
 let playerText = document.querySelector("div.player p");
 let computerText = document.querySelector("div.computer p");
 let roundText = document.querySelector("div.round-count p");
@@ -11,6 +13,16 @@ let roundText = document.querySelector("div.round-count p");
 playerText.textContent = `Your Score: ${playerScore}`;
 computerText.textContent = `Computer Score: ${computerScore}`;
 roundText.textContent = `Round Count: ${roundCount}`;
+
+let resetGame = () => {
+  resetText.textContent = "Refresh to play again!";
+};
+
+const disableButton = () => {
+  btnRock.disabled = true;
+  btnPaper.disabled = true;
+  btnScissors.disabled = true;
+};
 
 // player input
 let playerSelection = "";
@@ -54,44 +66,46 @@ const startGame = () => {
     if (player === computer) {
       playerScore++;
       computerScore++;
-      return "Huh.. a tie.  Go again!";
+      messageText.textContent = "Huh.. a tie.  Go again!";
     } else if (
       // player wins
       player === "rock" &&
       computer === "scissors"
     ) {
       playerScore++;
-      return "You win!  Rock beats scissors.";
+      messageText.textContent = "You win!  Rock beats scissors.";
     } else if (player === "paper" && computer === "rock") {
       playerScore++;
-      return "You win!  Paper beats rock.";
+      messageText.textContent = "You win!  Paper beats rock.";
     } else if (player === "scissors" && computer === "paper") {
       playerScore++;
-      return "You win!  Scissors beats paper.";
+      messageText.textContent = "You win!  Scissors beats paper.";
     } else if (
       // computer wins
       computer === "rock" &&
       player === "scissors"
     ) {
       computerScore++;
-      return "You lose!  Rock beats scissors.";
+      messageText.textContent = "You lose!  Rock beats scissors.";
     } else if (computer === "paper" && player === "rock") {
       computerScore++;
-      return "You lose!  Paper beats rock.";
+      messageText.textContent = "You lose!  Paper beats rock.";
     } else if (computer === "scissors" && player === "paper") {
       computerScore++;
-      return "You lose!  Scissors beats paper.";
+      messageText.textContent = "You lose!  Scissors beats paper.";
     }
   };
 
   let endGame = () => {
     if (playerScore > computerScore) {
-      return "GAME OVER!  You beat the computer.  Congrats!";
+      messageText.textContent = "GAME OVER!  You beat the computer.  Congrats!";
     } else if (playerScore < computerScore) {
-      return "GAME OVER!  You lose.  Better luck next time!";
+      messageText.textContent = "GAME OVER!  You lose.  Better luck next time!";
     } else {
-      return "GAME OVER!  Nice try, but we have a tie.";
+      messageText.textContent = "GAME OVER!  Nice try, but we have a tie.";
     }
+    disableButton();
+    resetGame();
   };
 
   playRound(playerSelection, getCompChoice());
